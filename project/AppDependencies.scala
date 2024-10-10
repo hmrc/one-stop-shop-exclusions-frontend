45914ae72ea8d275df0426a4b74b302a1e80fa39
Lee Powell <lee.powell@digital.hmrc.gov.uk>
2024-10-10 12:55:38 +0100

    VEOSS-1965 | Update library versions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,32 +3,32 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "8.4.0"
-  private val hmrcMongoVersion = "1.7.0"
-  private val httpVerbVersion = "15.0.0"
+  private val bootstrapVersion = "9.5.0"
+  private val hmrcMongoVersion = "2.2.0"
+  private val httpVerbVersion = "15.1.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "http-verbs-play-30"                     % httpVerbVersion,
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-30"             % bootstrapVersion,
-    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"             % "10.5.0",
-    "uk.gov.hmrc"             %% "play-conditional-form-mapping-play-30"  % "2.0.0",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"             % "10.13.0",
+    "uk.gov.hmrc"             %% "play-conditional-form-mapping-play-30"  % "3.2.0",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"                     % hmrcMongoVersion,
-    "uk.gov.hmrc"             %% "domain-play-30"                         % "9.0.0",
-    "org.typelevel"           %% "cats-core"                              % "2.9.0"
+    "uk.gov.hmrc"             %% "domain-play-30"                         % "10.0.0",
+    "org.typelevel"           %% "cats-core"                              % "2.12.0"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapVersion,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30"    % hmrcMongoVersion,
-    "org.scalatest"           %% "scalatest"                  % "3.2.15",
+    "org.scalatest"           %% "scalatest"                  % "3.2.19",
     "org.scalatestplus"       %% "scalacheck-1-15"            % "3.2.11.0",
     "org.scalatestplus"       %% "mockito-3-4"                % "3.2.10.0",
-    "org.mockito"             %% "mockito-scala"              % "1.17.30",
-    "org.scalacheck"          %% "scalacheck"                 % "1.17.0",
+    "org.mockito"             %% "mockito-scala"              % "1.17.37",
+    "org.scalacheck"          %% "scalacheck"                 % "1.18.0",
     "org.pegdown"             %  "pegdown"                    % "1.6.0",
-    "org.jsoup"               %  "jsoup"                      % "1.15.4",
-    "com.vladsch.flexmark"    %  "flexmark-all"               % "0.64.6",
-    "org.scalatestplus.play"  %% "scalatestplus-play"         % "5.1.0",
+    "org.jsoup"               %  "jsoup"                      % "1.18.1",
+    "com.vladsch.flexmark"    %  "flexmark-all"               % "0.64.8",
+    "org.scalatestplus.play"  %% "scalatestplus-play"         % "7.0.1",
     "org.playframework"       %% "play-test"                  % PlayVersion.current
   ).map(_ % "test, it")
 
