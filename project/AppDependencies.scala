0e940847c577c7a40209e00abd245adffa2f1e44
Dorothea87 <147050615+Dorothea87@users.noreply.github.com>
2025-10-13 10:23:24 +0100

    VEOSS-2138 update dependencies and warnings

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,16 +9,16 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-30"             % bootstrapVersion,
-    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"             % "12.8.0",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"             % "12.17.0",
     "uk.gov.hmrc"             %% "play-conditional-form-mapping-play-30"  % "3.3.0",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"                     % hmrcMongoVersion,
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"                     % "2.9.0",
     "uk.gov.hmrc"             %% "domain-play-30"                         % "10.0.0",
     "org.typelevel"           %% "cats-core"                              % "2.13.0"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapVersion,
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30"    % hmrcMongoVersion,
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30"    % "2.9.0",
     "org.scalatest"           %% "scalatest"                  % "3.2.19",
     "org.scalatestplus"       %% "scalacheck-1-15"            % "3.2.11.0",
     "org.scalacheck"          %% "scalacheck"                 % "1.18.1",
