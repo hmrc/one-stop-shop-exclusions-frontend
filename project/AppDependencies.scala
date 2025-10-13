182d9576f07fccd11b6571f6ea59e7aa182029ca
Dorothea87 <147050615+Dorothea87@users.noreply.github.com>
2025-10-13 12:23:04 +0100

    VEOSS-2138 update dependencies the right way

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,21 +4,21 @@ import sbt.*
 object AppDependencies {
 
   private val bootstrapVersion = "9.19.0"
-  private val hmrcMongoVersion = "2.7.0"
+  private val hmrcMongoVersion = "2.9.0"
   private val httpVerbVersion = "15.1.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-30"             % bootstrapVersion,
     "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"             % "12.17.0",
     "uk.gov.hmrc"             %% "play-conditional-form-mapping-play-30"  % "3.3.0",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"                     % "2.9.0",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"                     % hmrcMongoVersion,
     "uk.gov.hmrc"             %% "domain-play-30"                         % "10.0.0",
     "org.typelevel"           %% "cats-core"                              % "2.13.0"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapVersion,
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30"    % "2.9.0",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30"    % hmrcMongoVersion,
     "org.scalatest"           %% "scalatest"                  % "3.2.19",
     "org.scalatestplus"       %% "scalacheck-1-15"            % "3.2.11.0",
     "org.scalacheck"          %% "scalacheck"                 % "1.18.1",
