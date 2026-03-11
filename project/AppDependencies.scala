023ccbe0c5ee48876b3d6b5939e026e8e22fdb5e
Lee Powell <lee.powell@digital.hmrc.gov.uk>
2026-03-11 08:54:02 +0000

    VEI-815 | Update libraries to fix reported vulnerabilities and fix deprecated code

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,14 +3,14 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.19.0"
+  private val bootstrapVersion = "10.7.0"
   private val hmrcMongoVersion = "2.12.0"
   private val httpVerbVersion = "15.1.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-30"             % bootstrapVersion,
-    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"             % "12.29.0",
-    "uk.gov.hmrc"             %% "play-conditional-form-mapping-play-30"  % "3.4.0",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"             % "12.32.0",
+    "uk.gov.hmrc"             %% "play-conditional-form-mapping-play-30"  % "3.5.0",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"                     % hmrcMongoVersion,
     "uk.gov.hmrc"             %% "domain-play-30"                         % "10.0.0",
     "org.typelevel"           %% "cats-core"                              % "2.13.0"
@@ -20,7 +20,7 @@ object AppDependencies {
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapVersion,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30"    % hmrcMongoVersion,
     "org.scalatest"           %% "scalatest"                  % "3.2.19",
-    "org.scalatestplus"       %% "scalacheck-1-15"            % "3.2.11.0",
+    "org.scalatestplus"       %% "scalacheck-1-17"            % "3.2.18.0",
     "org.scalacheck"          %% "scalacheck"                 % "1.18.1",
     "org.jsoup"               %  "jsoup"                      % "1.21.1",
     "com.vladsch.flexmark"    %  "flexmark-all"               % "0.64.8",
