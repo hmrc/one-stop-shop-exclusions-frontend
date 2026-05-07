0bd15af0b4d71e0d868abec2efe7c5d8f8225559
Andrew Smith <andy@andyroo.info>
2026-05-07 15:53:30 +0100

    VEOSS-2176 update frontend version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,7 +9,7 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-30"             % bootstrapVersion,
-    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"             % "12.32.0",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"             % "13.5.0",
     "uk.gov.hmrc"             %% "play-conditional-form-mapping-play-30"  % "3.5.0",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"                     % hmrcMongoVersion,
     "uk.gov.hmrc"             %% "domain-play-30"                         % "13.0.0",
